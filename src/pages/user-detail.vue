e9437c26fe2da93e422a5f706dbdacd231952f89
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-01-05 18:20:00 +0900

    ✨ add user-detail page

---
+++ src/pages/user-detail.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useLoginUser } from '../store/user';
+
+const user = useLoginUser();
+</script>
+
+<template>
+  <div>
+    <p>ユーザーID: {{ user.id }}</p>
+    <p>ユーザー名: {{ user.name }}</p>
+    <p>メールアドレス: {{ user.mail }}</p>
+  </div>
+</template>
